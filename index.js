1a321eb3327184c9b801a3de27a910d959623b34
luis patino <lcp.patino@gmail.com>
2023-04-09 16:26:39 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = function(num1, num2) {
+    return num1 / num2;
+  }
+  
+  console.log(divide(10, 2));
+  
+  const square = (num) => {
+    return num * num;
+  }
+  
+  console.log(square(5));
+  
+  const add = (num1, num2) => {
+    return num1 + num2;
+  }
+  
+  console.log(add(5, 10));
+  
